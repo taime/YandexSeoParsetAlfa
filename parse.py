49a06cf5412419f1c7099dd92001c5821364c730
tema <regibox@icloud.com>
2020-05-05 22:27:50 +0300

    proxies

--- parse.py
+++ parse.py
@@ -12,68 +12,89 @@ import http.client
 
 # proxies = {"http": "http://192.41.71.204:3128"}
 
-proxies_list = ["http://192.41.71.204:3128",
-                "http://159.65.255.142:3128",
-                "http://96.234.124.154:80",
-                "http://3.21.117.16:80",
-                "http://24.172.82.94:53281",
-                "http://173.192.128.238:25",
-                "http://80.187.140.26:8080",
-                "http://159.8.114.34:8123",
-                "http://176.103.48.116:1182",
-                "http://52.166.247.251:80",
-                "http://92.79.65.240:8080",
-                "http://79.137.123.252:3129",
-                "http://200.52.141.162:53281",
-                "http://167.86.96.193:80",
-                "http://169.57.157.148:8123",
-                "http://138.68.53.44:8118",
-                "http://125.167.100.150:80",
-                "http://51.77.149.1:3133",
-                "http://79.137.123.155:3129",
-                "http://54.37.14.65:3129",
-                "http://91.150.189.122:43102",
-                "http://198.50.152.64:23500",
-                "http://138.36.2.186:45277",
-                "http://144.217.101.242:3129",
-                "http://191.100.128.158:21776",
-                "http://91.210.169.241:80",
-                "http://185.56.8.209:84",
-                "http://181.118.167.104:80",
-                "http://190.84.232.87:49907",
-                "http://14.38.255.39:80",
-                "http://189.57.62.146:80",
-                "http://185.56.209.114:51386",
-                "http://31.179.224.42:38263",
-                "http://94.130.179.24:8045",
-                ]
+proxies_list_http = ["http://192.41.71.204:3128",
+                     "http://159.65.255.142:3128",
+                     "http://96.234.124.154:80",
+                     "http://3.21.117.16:80",
+                     "http://24.172.82.94:53281",
+                     "http://173.192.128.238:25",
+                     "http://80.187.140.26:8080",
+                     "http://159.8.114.34:8123",
+                     "http://176.103.48.116:1182",
+                     "http://52.166.247.251:80",
+                     "http://92.79.65.240:8080",
+                     "http://79.137.123.252:3129",
+                     "http://200.52.141.162:53281",
+                     "http://167.86.96.193:80",
+                     "http://169.57.157.148:8123",
+                     "http://138.68.53.44:8118",
+                     "http://125.167.100.150:80",
+                     "http://51.77.149.1:3133",
+                     "http://79.137.123.155:3129",
+                     "http://54.37.14.65:3129",
+                     "http://91.150.189.122:43102",
+                     "http://198.50.152.64:23500",
+                     "http://138.36.2.186:45277",
+                     "http://144.217.101.242:3129",
+                     "http://191.100.128.158:21776",
+                     "http://91.210.169.241:80",
+                     "http://185.56.8.209:84",
+                     "http://181.118.167.104:80",
+                     "http://190.84.232.87:49907",
+                     "http://14.38.255.39:80",
+                     "http://189.57.62.146:80",
+                     "http://185.56.209.114:51386",
+                     "http://31.179.224.42:38263",
+                     "http://94.130.179.24:8045",
+                     ]
 
-
-def getRandomProxy():
-    q_proxies = len(proxies_list)-1
-    random = randint(0, q_proxies)
-    proxies = {"http": proxies_list[random]}
-    return proxies
-
-# proxies = ['167.114.167.143:']
+proxies_list_https = ["https://74.59.132.126:49073",
+                      "https://128.199.184.216:8080",
+                      "https://212.83.190.118:5836",
+                      "https://212.83.189.137:5836",
+                      "https://212.83.188.253:5836",
+                      "https://212.83.185.168:5836",
+                      "https://212.83.179.105:5836",
+                      "https://167.86.66.178:3128",
+                      "https://212.83.166.175:5836",
+                      "https://212.129.62.202:5836",
+                      "https://212.129.58.162:5836",
+                      "https://212.129.57.222:5836",
+                      "https://212.129.43.124:5836",
+                      "https://212.129.42.175:5836",
+                      "https://212.129.38.35:5836",
+                      "https://212.129.28.152:5836",
+                      "https://212.129.27.246:5836",
+                      "https://118.99.95.117:8080",
+                      "https://163.172.229.128:5836",
+                      "https://194.8.146.167:50510",
+                      "https://171.6.147.25:8213",
+                      "https://212.129.5.123:5836",
+                      "https://102.176.160.107:8080",
+                      "https://198.27.76.4:5007",
+                      "https://195.154.62.117:5836",
+                      "https://195.154.51.238:5836",
+                      "https://195.154.59.207:5836",
+                      "https://195.154.58.65:5836",
+                      "https://195.154.33.119:5836"]
 
 
 files_folder = './tmp/'
 # domain = 'gurmanit.ru'
 domain = 'et-serv.ru'
-# phrases = [
-#     'томаты пилати что это',
-#     'паста трофи',
-#     'каперсы на веточке',
-#     'пекорино с трюфелем',
-#     'салями с трюфелем',
-#     'помидоры пилати',
-#     'сыр камамбер шввейцария',
-#     'сыр бри с трюфелями купить',
-#     'паста трофи',
-#     '90,5 % колбаса с трюфелем италия',
-#     'чернила каракатицы купить',
-# ]
+phrases_g = [
+    'томаты пилати что это',
+    'паста трофи',
+    'каперсы на веточке',
+    'пекорино с трюфелем',
+    'салями с трюфелем',
+    'помидоры пилати',
+    'сыр камамбер шввейцария',
+    'сыр бри с трюфелями купить',
+    'паста трофи',
+    '90,5 % колбаса с трюфелем италия',
+    'чернила каракатицы купить',
+]
 phrases = [
     'частотный преобразователь danfoss',
     'частотный преобразователь данфосс',
@@ -145,6 +166,18 @@ hdr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKi
 cookies = chrome_cookies(url)
 
 
+def getRandomProxy():
+    proxies_list = proxies_list_http
+    # proxies_list = proxies_list_https
+
+    q_proxies = len(proxies_list)-2
+    random = randint(0, q_proxies)
+    proxies = {"http": proxies_list[random]}
+    # proxies = {"https": proxies_list[random]}
+
+    return proxies
+
+
 def strP(pos):
     if pos < 10:
         return "  " + str(pos)
@@ -201,7 +234,13 @@ def getPageWithProxy(domain, text, url, p, hdr, cookies, tries=0):
         response.raise_for_status()
         addProxyToTxtFile(proxies)
         whiteHtmlFile(response.content, domain, text, url, p)
-        return(response)
+        if response.text is None:
+            print("reponse text is None. Let's try another proxy")
+            continueGetIfNotTooManyTries(domain, text, url, p, hdr, cookies, tries)
+        else:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            return(soup)
+
     except requests.exceptions.HTTPError as errh:
         printy("ERROR HTTP ", "rB")
         print("Http Error:", errh)
@@ -288,11 +327,10 @@ def checkPhrase(text):
     clearTxtFile(domain, text)
     while (page < end_page):
         # response = getPage(domain, text, url, page, hdr, cookies)
-
-        # response = getPageWithProxy(domain, text, url, page, hdr, cookies)
-        # soup = BeautifulSoup(response.text, 'html.parser')
-
-        soup = getSoupFromHtmlPage('./tmp/et-serv.ru_частотный преобразователь 380_0.html')
+        # response_text = getPageWithProxy(domain, text, url, page, hdr, cookies)
+        soup = getPageWithProxy(domain, text, url, page, hdr, cookies)
+        # soup = BeautifulSoup(response_text, 'html.parser')
+        # soup = getSoupFromHtmlPage('./tmp/et-serv.ru_частотный преобразователь 380_0.html')
 
         if soup is None:
             printy("Soup is None", "m")
@@ -301,34 +339,34 @@ def checkPhrase(text):
             printy("The Capcha :(", "m")
             return("STOP")
         else:
-            print("We have Soup!")
             res = parseSearchPage(soup, domain, text, page)
             addPositionsToTxtFile(res['words'], domain, text)
             page += 1
             if res["didFind"]:
                 printy("Great! We've found it!", "m")
-                return(True)
+                # return(True)
                 break
-            else:
-                return("Didn't found the word on this page, will try another")
+            # else:
+            #     return("Didn't found the word on this page, will try another")
 
 
 def doTheJob():
     printy("[wB]   -------------------\n       " + domain + "\n   -------------------")
+    wl = len(phrases)-1
 
     for word in phrases:
-        res = checkPhrase(word)
-        # # TEMPORARY TAKE RANDOM NUMBER FO DICT
-        # random_word = phrases[randint(0, len(phrases)-1)]
-        # res = checkPhrase(random_word)
+        # res = checkPhrase(word)
+        # TEMPORARY TAKE RANDOM NUMBER FO DICT
+        random_word = phrases[randint(0, wl)]
+        res = checkPhrase(random_word)
         if res == "STOP":
             print("Had to stop program!")
             break
-        elif res:
-            print("Great Job. Let's find another word!")
-        else:
-            printy("res is", "yB")
-            print(res)
+        # elif res:
+        #     print("Great Job. Let's find another word!")
+        # else:
+        #     printy("res is", "yB")
+        #     print(res)
 
 
 doTheJob()
